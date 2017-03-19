d41ae96b4e4e962705048138e76944670e02870f
rafaelrpaiva <rafaelpaiva@gmail.com>
2017-03-19 01:07:24 -0300

    Solution for Nanodegree - correcting Naked Twins.

--- solution.py
+++ solution.py
@@ -23,9 +23,13 @@ diagonal_units = list()
 diagonal_units.append([rs+cs for rs, cs in zip(rows, cols)])
 diagonal_units.append([rs+cs for rs, cs in zip(rows, cols[::-1])])
 
-unit_list = row_units + column_units + square_units + diagonal_units
+unit_list = row_units + column_units + square_units
 units = dict((s, [u for u in unit_list if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
+
+diag_unit_list = row_units + column_units + square_units + diagonal_units
+diag_units = dict((s, [u for u in unit_list if s in u]) for s in boxes)
+diag_peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
 
 
 def assign_value(values, box, value):
@@ -39,7 +43,7 @@ def assign_value(values, box, value):
     return values
 
 
-def naked_twins(values, box):
+def naked_twins(values):
     """Eliminate values using the naked twins strategy.
     Args:
         values(dict): a dictionary of the form {'box_name': '123456789', ...}
@@ -49,21 +53,27 @@ def naked_twins(values, box):
     Returns:
         the values dictionary with the naked twins eliminated from peers.
     """
-    for unit in unit_list:
-        # Creating a set makes things easier to find intersections between boxes
-        unit = set(unit)
-        set_of_peers = set(peers[box])
-        # Find all instances of naked twins
-        for peer in unit.intersection(set_of_peers):
-            if not set(values[peer]).difference(set(values[box])):
-                digit1 = values[box][0]
-                digit2 = values[box][1]
-                # Eliminate the naked twins as possibilities for their peers
-                for item in unit.difference(set([box, peer])):
-                    if digit1 in values[item]:
-                        values[item].remove(digit1)
-                    if digit2 in values[item]:
-                        values[item].remove(digit2)
+    ntwins = []
+    # Part 1: filter the boxes and peers to find the instances of naked twins in the present board
+    for box in values:
+        if len(values[box]) == 2:
+            for peer in peers[box]:
+                if box < peer and values[peer] == values[box]:
+                    ntwins.append([box, peer])
+    # Part 2: eliminate naked twins values
+    for n in ntwins:
+        nt_units = [u for u in unit_list if n[0] in u and n[1] in u]
+        for each_unit in nt_units:
+            # Eliminate the naked twins as possibilities
+            for item in each_unit:
+                if item != n[0] and item != n[1]:
+                    values[item] = values[item].replace(values[n[0]][0], '')
+                    assign_value(values, item, values[item])  # to visualize the data
+
+                    values[item] = values[item].replace(values[n[0]][1], '')
+                    assign_value(values, item, values[item])  # to visualize the data
+    if len([box for box in values.keys() if len(values[box]) == 0]):
+        return False
     return values
 
 
@@ -118,18 +128,15 @@ def eliminate(values):
     solved_values = [box for box in values.keys() if len(values[box]) == 1]
     for box in solved_values:
         number_assigned = values[box]
-        for peer in peers[box]:
+        for peer in diag_peers[box]:
             values[peer] = values[peer].replace(number_assigned, '')
         if len(values[box]) == 1:
             assign_value(values, box, values[box])
-        # if only two digits remain in a box, it's time to try naked_twins
-        elif len(values[box]) == 2:
-            naked_twins(values, box)
     return values
 
 
 def only_choice(values):
-    for unit in unit_list:
+    for unit in diag_unit_list:
         for digit in '123456789':
             dplaces = [box for box in unit if digit in values[box]]
             if len(dplaces) == 1:
@@ -144,6 +151,8 @@ def reduce_puzzle(values):
         solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
         # Strategy 1: Eliminate
         values = eliminate(values)
+        # Strategy 2: Naked Twins
+        values = naked_twins(values)
         # Strategy 2: Only Choice
         values = only_choice(values)
         # Check how many boxes have a determined value and compare
@@ -190,21 +199,10 @@ def solve(grid):
     return search(values)
 
 
-def solve_regular(grid):
-    unit_list = row_units + column_units + square_units + diagonal_units
-    units = dict((s, [u for u in unit_list if s in u]) for s in boxes)
-    peers = dict((s, set(sum(units[s], [])) - set([s])) for s in boxes)
-    return solve(grid)
-
-
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(solve(diag_sudoku_grid))
 
-    """
-    common_sudoku_grid = '..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..'
-    display(grid_values(common_sudoku_grid))
-    display(solve_regular(common_sudoku_grid))
     naked_twins_grid = {'I6': '4', 'H9': '3', 'I2': '6', 'E8': '1', 'H3': '5', 'H7': '8', 'I7': '1', 'I4': '8',
                             'H5': '6', 'F9': '7', 'G7': '6', 'G6': '3', 'G5': '2', 'E1': '8', 'G3': '1', 'G2': '8',
                             'G1': '7', 'I1': '23', 'C8': '5', 'I3': '23', 'E5': '347', 'I5': '5', 'C9': '1', 'G9': '5',
@@ -216,9 +214,29 @@ if __name__ == '__main__':
                             'D2': '1', 'H1': '4', 'H6': '17', 'H2': '9', 'H4': '17', 'D3': '2379', 'B4': '27',
                             'B5': '1', 'B6': '8', 'B7': '27', 'E9': '2', 'B1': '9', 'B2': '5', 'B3': '6', 'D6': '279',
                             'D7': '34', 'D4': '237', 'D5': '347', 'B8': '3', 'B9': '4', 'D1': '5'}
+    possible_solutions_1 = [
+        {'G7': '6', 'G6': '3', 'G5': '2', 'G4': '9', 'G3': '1', 'G2': '8', 'G1': '7', 'G9': '5', 'G8': '4', 'C9': '1',
+         'C8': '5', 'C3': '8', 'C2': '237', 'C1': '23', 'C7': '9', 'C6': '6', 'C5': '37', 'A4': '2357', 'A9': '8',
+         'A8': '6', 'F1': '6', 'F2': '4', 'F3': '23', 'F4': '1235', 'F5': '8', 'F6': '125', 'F7': '35', 'F8': '9',
+         'F9': '7', 'B4': '27', 'B5': '1', 'B6': '8', 'B7': '27', 'E9': '2', 'B1': '9', 'B2': '5', 'B3': '6', 'C4': '4',
+         'B8': '3', 'B9': '4', 'I9': '9', 'I8': '7', 'I1': '23', 'I3': '23', 'I2': '6', 'I5': '5', 'I4': '8', 'I7': '1',
+         'I6': '4', 'A1': '1', 'A3': '4', 'A2': '237', 'A5': '9', 'E8': '1', 'A7': '27', 'A6': '257', 'E5': '347',
+         'E4': '6', 'E7': '345', 'E6': '579', 'E1': '8', 'E3': '79', 'E2': '37', 'H8': '2', 'H9': '3', 'H2': '9',
+         'H3': '5', 'H1': '4', 'H6': '17', 'H7': '8', 'H4': '17', 'H5': '6', 'D8': '8', 'D9': '6', 'D6': '279',
+         'D7': '34', 'D4': '237', 'D5': '347', 'D2': '1', 'D3': '79', 'D1': '5'},
+        {'I6': '4', 'H9': '3', 'I2': '6', 'E8': '1', 'H3': '5', 'H7': '8', 'I7': '1', 'I4': '8', 'H5': '6', 'F9': '7',
+         'G7': '6', 'G6': '3', 'G5': '2', 'E1': '8', 'G3': '1', 'G2': '8', 'G1': '7', 'I1': '23', 'C8': '5', 'I3': '23',
+         'E5': '347', 'I5': '5', 'C9': '1', 'G9': '5', 'G8': '4', 'A1': '1', 'A3': '4', 'A2': '237', 'A5': '9',
+         'A4': '2357', 'A7': '27', 'A6': '257', 'C3': '8', 'C2': '237', 'C1': '23', 'E6': '579', 'C7': '9', 'C6': '6',
+         'C5': '37', 'C4': '4', 'I9': '9', 'D8': '8', 'I8': '7', 'E4': '6', 'D9': '6', 'H8': '2', 'F6': '125',
+         'A9': '8', 'G4': '9', 'A8': '6', 'E7': '345', 'E3': '79', 'F1': '6', 'F2': '4', 'F3': '23', 'F4': '1235',
+         'F5': '8', 'E2': '3', 'F7': '35', 'F8': '9', 'D2': '1', 'H1': '4', 'H6': '17', 'H2': '9', 'H4': '17',
+         'D3': '79', 'B4': '27', 'B5': '1', 'B6': '8', 'B7': '27', 'E9': '2', 'B1': '9', 'B2': '5', 'B3': '6',
+         'D6': '279', 'D7': '34', 'D4': '237', 'D5': '347', 'B8': '3', 'B9': '4', 'D1': '5'}
+        ]
 
-    display(solve(naked_twins_grid))
-    """
+    if naked_twins(naked_twins_grid) in possible_solutions_1:
+        print("Naked twins is working!")
 
     try:
         from visualize import visualize_assignments
